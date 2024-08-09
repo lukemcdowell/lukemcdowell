907fc3b691af3bbba4a218e9fd1f55554f72b1a0
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-08-09 15:23:06 +0100

    adding initial gallery, reading from firebase

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,64 @@
+import { getDownloadURL, listAll, ref } from 'firebase/storage';
 import * as React from 'react';
+import { imageStore } from '../../firebase-config';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 import '../styles/gallery.css';
 
 const Gallery = () => {
+  const [imageUrls, setImageUrls] = React.useState([]);
+
+  const splitIntoThree = (array) => {
+    const firstArray = array.slice(0, Math.ceil(array.length / 3));
+    const secondArray = array.slice(
+      Math.ceil(array.length / 3),
+      Math.ceil((2 * array.length) / 3)
+    );
+    const thirdArray = array.slice(Math.ceil((2 * array.length) / 3));
+
+    return [firstArray, secondArray, thirdArray];
+  };
+
+  const fetchImages = async () => {
+    const storageRef = ref(imageStore, 'images/');
+
+    const response = await listAll(storageRef);
+    const urlPromises = response.items.map((imageRef) =>
+      getDownloadURL(imageRef)
+    );
+
+    return Promise.all(urlPromises);
+  };
+
+  const getImageUrls = async () => {
+    const urls = await fetchImages();
+    setImageUrls(urls);
+  };
+
+  React.useEffect(() => {
+    getImageUrls();
+  });
+
   return (
     <Layout breadcrumbPath={['home', 'gallery']}>
       <section className="container">
         <h1 className="heading">Gallery</h1>
-        <div className="gallery-container">test</div>
+        {imageUrls.length !== 0 && (
+          <div className="gallery-container">
+            {splitIntoThree(imageUrls).map((array, index) => (
+              <div key={index} className="gallery-column">
+                {array.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={index}
+                    className="gallery-image"
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </section>
     </Layout>
   );
